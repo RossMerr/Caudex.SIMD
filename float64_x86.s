f1aace4ec59480f80e108dc15ec8ab65f87c3e14
Ross Merrigan <email@rossmerrigan.com>
2018-02-27 22:02:30 +0100

    cleanup to whitespace

--- float64_x86.s
+++ float64_x86.s
@@ -10,34 +10,34 @@
 // MOVOU is MOVDQU.
 
 // func addFloat64(x, y []float64) []float64
-TEXT ·addFloat64(SB), NOSPLIT, $0-48	
-    MOVQ a+0(FP), SI
+TEXT ·addFloat64(SB), NOSPLIT, $0-48
+	MOVQ a+0(FP), SI
 	MOVQ b+24(FP), DI
 	MOVOU (SI), X
 	MOVOU (DI), y
 	ADDPD y, x
-    MOVOU x, (SI)
+	MOVOU x, (SI)
 	MOVOU y, (DI)
 	RET
 
 // func subtractFloat64(x, y []float64) []float64
-TEXT ·subtractFloat64(SB), NOSPLIT, $0-48	
-    MOVQ a+0(FP), SI
+TEXT ·subtractFloat64(SB), NOSPLIT, $0-48
+	MOVQ a+0(FP), SI
 	MOVQ b+24(FP), DI
 	MOVOU (SI), X
 	MOVOU (DI), y
 	SUBPD y, x
-    MOVOU x, (SI)
+	MOVOU x, (SI)
 	MOVOU y, (DI)
-	RET	
+	RET
 
 // func multiplyFloat64(x, y []float64) []float64
-TEXT ·multiplyFloat64(SB), NOSPLIT, $0-48	
-    MOVQ a+0(FP), SI
+TEXT ·multiplyFloat64(SB), NOSPLIT, $0-48
+	MOVQ a+0(FP), SI
 	MOVQ b+24(FP), DI
 	MOVOU (SI), X
 	MOVOU (DI), y
 	MULPD y, x
-    MOVOU x, (SI)
+	MOVOU x, (SI)
 	MOVOU y, (DI)
-	RET		
+	RET
